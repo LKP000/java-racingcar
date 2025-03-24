e85a0d8e5f6399fe87ad8b561e352baae2b092bc
LKP <rud79730@gmail.com>
2025-03-24 21:15:33 +0900

    feat: Car 이름 반환 기능 추가

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -2,10 +2,14 @@ package racingcar;
 
 public class Car {
     private String name;
-    
+
     public Car(String s) {
         if(s.length() > 5) throw new IllegalArgumentException();
         name = s;
         location = 0;
+    }
+
+    public String getName() {
+        return name;
     }
 }
