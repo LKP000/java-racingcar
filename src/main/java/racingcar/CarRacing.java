cd3f4c01f8149f2e53a65c1de002749bc4f64650
LKP <rud79730@gmail.com>
2025-03-24 21:32:07 +0900

    feat: 우승자 판정 기능 추가

--- src/main/java/racingcar/CarRacing.java
+++ src/main/java/racingcar/CarRacing.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 
 public class CarRacing {
     private List<Car> cars = new ArrayList<>();
+    private List<String> winners = new ArrayList<>();
     private int round;
 
     public CarRacing() {}
@@ -23,6 +24,15 @@ public class CarRacing {
     public void moveCars() {
         for (Car car : cars) {
             car.move();
+        }
+    }
+
+    public void referee() {
+        cars.sort((Car o1, Car o2) -> o2.getLocation() - o1.getLocation());
+        int max = cars.get(0).getLocation();
+        for (Car car : cars) {
+            if (car.getLocation() >= max) winners.add(car.getName());
+            else break;
         }
     }
 }
